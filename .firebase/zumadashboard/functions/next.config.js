75002d222d15edd396133036ddf6033af19f26ba
oscy <oscy@papis-macbook.local>
2023-05-01 23:09:25 -0700

    first stage of dash

--- .firebase/zumadashboard/functions/next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-var nextConfig = {
-  reactStrictMode: true,
-  env: {
-    secret: 'f722820f40920467ada1c915bc260c321adce0142fe96b12e293addf9694faea"',
-    NEXTAUTH_URL: "http://locahost:3000"
-  }
-};
-module.exports = nextConfig;
