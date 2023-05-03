bab2bf2fa909d7f331df09ea26bb82bacd128909
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2023-05-02 21:54:11 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     secret: 'f722820f40920467ada1c915bc260c321adce0142fe96b12e293addf9694faea',
-    NEXTAUTH_URL: "http://http://34.220.205.154:3000",
+    NEXTAUTH_URL: "http://34.220.205.154:3000",
     basePath: '/',
     
   }
