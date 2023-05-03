5261df2b4dba533d66a4ac7432f4894478588c47
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2023-05-02 21:53:35 -0700

    added url paths

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   env:{
     secret: 'f722820f40920467ada1c915bc260c321adce0142fe96b12e293addf9694faea',
-
+    NEXTAUTH_URL: "http://http://34.220.205.154:3000",
     basePath: '/',
     
   }
